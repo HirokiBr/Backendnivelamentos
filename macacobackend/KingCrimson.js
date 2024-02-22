caf595be212d315dcc9afccf9974ba77b3e6b2a9
HirokiNatsuki/Victor <126178673+HirokiBr@users.noreply.github.com>
2024-02-22 08:25:19 -0300

    Add files via upload

---
+++ macacobackend/KingCrimson.js
@@ -0,0 +1,20 @@
+let Contador = 0;
+let parada = "false";
+
+while(parada){
+console.log(contador);
+contador++;
+
+}
+contador = 0;
+do {
+    console.log(`Loop Do - $`{contador});
+    contador++
+} while(parada);
+contador + 0;
+
+//loop FOR
+for(let i = 0; i < 10; i++){
+    console.log(`Loop For - ${i++}`)
+
+}
---
+++ macacobackend/jojo.js
@@ -0,0 +1,11 @@
+let nome;
+nome = prompt("digite seu nome:");
+console.log(nome);
+window.alert(`ola ${nome}!`);
+let numero = 19;
+let palpite = prompt("Digite um numero de 0 a 20:");
+console.log(palpite);
+if(numero == palpite ){
+window.alert (`Parabens vc é um idiota ${nome} o numero era ${numero}, e voce acertou!!`);
+
+}
